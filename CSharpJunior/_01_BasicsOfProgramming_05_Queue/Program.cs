0f5287fc7ab176802723a93b299c44a8c3e6a3aa
Nikolay Granenkov <nickgran@yandex.ru>
2021-07-05 14:55:41 +0300

    fix: comments is evil

--- CSharpJunior/_01_BasicsOfProgramming_05_Queue/Program.cs
+++ CSharpJunior/_01_BasicsOfProgramming_05_Queue/Program.cs
@@ -7,17 +7,15 @@ namespace _01_BasicsOfProgramming_05_Queue
         static void Main(string[] args)
         {
             const uint minutestInHour = 60;
-            // time in minutes
-            uint timeOfReceipt = 10;
+            uint timeOfReceiptInMinutes = 10;
 
             Console.Write("Введите количество людей в очереди, которое вы видите перед собой: ");
             uint peopleInQueue = Convert.ToUInt32(Console.ReadLine());
 
-            // time in minutes
-            uint waitingTime = peopleInQueue * timeOfReceipt;
+            uint waitingTimeInMinutes = peopleInQueue * timeOfReceiptInMinutes;
 
-            uint hours = waitingTime / minutestInHour;
-            uint minutes = waitingTime % minutestInHour;
+            uint hours = waitingTimeInMinutes / minutestInHour;
+            uint minutes = waitingTimeInMinutes % minutestInHour;
 
             Console.WriteLine($"Время вашего ожидание в очереди составляет {hours} часов {minutes} минут.");
         }
