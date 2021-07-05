298786f94f719f18ca82d6322983054b8036ea11
Nikolay Granenkov <nickgran@yandex.ru>
2021-07-05 14:15:13 +0300

    add: 05 queue

---
+++ CSharpJunior/_01_BasicsOfProgramming_05_Queue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01_BasicsOfProgramming_05_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const uint minutestInHour = 60;
+            // time in minutes
+            uint timeOfReceipt = 10;
+
+            Console.Write("Введите количество людей в очереди, которое вы видите перед собой: ");
+            uint peopleInQueue = Convert.ToUInt32(Console.ReadLine());
+
+            // time in minutes
+            uint waitingTime = peopleInQueue * timeOfReceipt;
+
+            uint hours = waitingTime / minutestInHour;
+            uint minutes = waitingTime % minutestInHour;
+
+            Console.WriteLine($"Время вашего ожидание в очереди составляет {hours} часов {minutes} минут.");
+        }
+    }
+}
