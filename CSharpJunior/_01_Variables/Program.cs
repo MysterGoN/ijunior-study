33e50f8f04de5153f530fa0d64a0dcf58f2a9fad
Nikolay Granenkov <nickgran@yandex.ru>
2021-07-05 12:05:08 +0300

    add: variable practice

---
+++ CSharpJunior/_01_Variables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint people;
+            // Возможно отрицательное значение, обозначает долг
+            int money;
+            bool isGateClose;
+            
+            uint rats;
+            uint sick;
+
+            char citySymbol;
+            string flagColor;
+            string nameOfMayor;
+
+            uint daysLeft;
+
+            float difficultyCoefficient;
+        }
+    }
+}
