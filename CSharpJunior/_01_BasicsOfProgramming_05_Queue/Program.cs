794787e69b3ae9d3088f45cf53df428a5e066e14
Nikolay Granenkov <nickgran@yandex.ru>
2021-07-05 15:13:15 +0300

    add: annotation

--- CSharpJunior/_01_BasicsOfProgramming_05_Queue/Program.cs
+++ CSharpJunior/_01_BasicsOfProgramming_05_Queue/Program.cs
@@ -4,18 +4,24 @@ namespace _01_BasicsOfProgramming_05_Queue
 {
     class Program
     {
+        /// <summary>
+        /// В рамках данного кода все переменные завязанные на время хранят значение в Минутах:
+        ///     - timeOfReceipt
+        ///     - waitingTime
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             const uint minutestInHour = 60;
-            uint timeOfReceiptInMinutes = 10;
+            uint timeOfReceipt = 10;
 
             Console.Write("Введите количество людей в очереди, которое вы видите перед собой: ");
             uint peopleInQueue = Convert.ToUInt32(Console.ReadLine());
 
-            uint waitingTimeInMinutes = peopleInQueue * timeOfReceiptInMinutes;
+            uint waitingTime = peopleInQueue * timeOfReceipt;
 
-            uint hours = waitingTimeInMinutes / minutestInHour;
-            uint minutes = waitingTimeInMinutes % minutestInHour;
+            uint hours = waitingTime / minutestInHour;
+            uint minutes = waitingTime % minutestInHour;
 
             Console.WriteLine($"Время вашего ожидание в очереди составляет {hours} часов {minutes} минут.");
         }
