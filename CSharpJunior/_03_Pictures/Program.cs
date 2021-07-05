8435e65e68e5ef32241fda9be3ec80ae47444f7b
Nikolay Granenkov <nickgran@yandex.ru>
2021-07-05 13:29:08 +0300

    add: pictures task

---
+++ CSharpJunior/_03_Pictures/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03_Pictures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint pictures = 52;
+            uint columns = 3;
+
+            uint rows = pictures / columns;
+            uint remainder = pictures % columns;
+            
+            
+            Console.WriteLine($"У вас имеется {pictures} картинки.");
+            Console.WriteLine($"Их необходимо разложить по {columns} колонкам.");
+            Console.WriteLine($"В итоге будет полностью заполнено {rows} рядов. В остатке будет {remainder} картинка.");
+        }
+    }
+}
