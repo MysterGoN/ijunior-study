49798de068b9ff6b18763d14f3823892aeaccbcb
Nikolay Granenkov <nickgran@yandex.ru>
2021-07-06 17:27:08 +0300

    part of 06 task

--- CSharpJunior/_02_ConditionalsAndLoops_06_NameOutput/Program.cs
+++ CSharpJunior/_02_ConditionalsAndLoops_06_NameOutput/Program.cs
@@ -6,7 +6,22 @@ namespace _02_ConditionalsAndLoops_06_NameOutput
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name;
+            char ornament;
+            
+            Console.WriteLine("Вы захотели заказать табличку с вашим именем на дверь. " +
+                              "Для этого вы пришли в мастерскую.");
+            
+            Console.WriteLine("Какое имя вы попросите выбить на табличке.");
+            Console.Write("Введите имя: ");
+            name = Console.ReadLine();
+            
+            Console.WriteLine("Какой орнамент вы выберете?");
+            Console.Write("Введите символ: ");
+            ornament = Convert.ToChar(Console.ReadKey());
+            
+            Console.WriteLine("Любуйтесь!");
+            
         }
     }
 }
