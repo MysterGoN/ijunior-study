981b7718e4a32a6a20210b3cb0d9e55e0b5e30df
Nikolay Granenkov <nickgran@yandex.ru>
2021-07-06 16:57:10 +0300

    add: 02 exit control programm

--- CSharpJunior/_02_ConditionalsAndLoops_02_ExitControl/Program.cs
+++ CSharpJunior/_02_ConditionalsAndLoops_02_ExitControl/Program.cs
@@ -4,9 +4,40 @@ namespace _02_ConditionalsAndLoops_02_ExitControl
 {
     class Program
     {
+        /// <summary>
+        /// Задачу можно решить также использую условия выхода внутри цикла и оператор break.
+        /// </summary>
+        /// <example>
+        /// while (true)
+        /// {
+        ///     if (action == exitPhrase)
+        ///     {
+        ///         break;
+        ///     }
+        ///     ...
+        /// }
+        /// </example>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string exitPhrase = "Выпусти меня!";
+            uint daysOfImprisonment = 1;
+            string action = "";
+            
+            Console.WriteLine("Смертный! Ты оказался заточен в моём пространстве!" +
+                              "\nОтсюда нет выхода, иди куда хочешь, делай что хочешь!" +
+                              "\nНо вдруг, если тебе надоест, ты можешь встать на колени и слёзно умолять меня " +
+                              $"выпустить тебя словами \"{exitPhrase}\"." +
+                              "\nИ может быть я смилостивлюсь и выпущу тебя отсюда!");
+
+            while (action != exitPhrase)
+            {
+                Console.WriteLine($"Вы в заточении уже {daysOfImprisonment++} дней." +
+                                  "\nЧем хотите заняться?" +
+                                  $"\nИли может быть хотите закончить? Вам всего лишь надо сказать \"{exitPhrase}\".");
+                Console.Write($"Введите любое действие или фразу \"{exitPhrase}\": ");
+                action = Console.ReadLine();
+            }
         }
     }
 }
