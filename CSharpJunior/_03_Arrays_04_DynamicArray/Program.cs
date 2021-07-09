9585510f7159fbcffd816cc7b997f6ea78091f99
Nikolay Granenkov <nickgran@yandex.ru>
2021-07-09 14:08:52 +0300

    add: module 3 task 4

--- CSharpJunior/_03_Arrays_04_DynamicArray/Program.cs
+++ CSharpJunior/_03_Arrays_04_DynamicArray/Program.cs
@@ -6,7 +6,71 @@ namespace _03_Arrays_04_DynamicArray
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string sumCommand = "sum";
+            const string exitCommand = "exit";
+            const int arrayDownPosition = 5;
+            int[] numbers = new int[0];
+            bool canExit = false;
+            string message = null;
+            
+            while (!canExit)
+            {
+                Console.Clear();
+                Console.WriteLine("Работа с консолью:" +
+                                  "\n  - Вы можете ввести целое число, чтобы добавить его в массив" +
+                                  $"\n  - Вы можете ввести команду {sumCommand}, что позволит сложить все числа массива" +
+                                  $"\n  - Вы можете ввести команду {exitCommand}, для выхода из программы");
+                
+                Console.Write("\nВведите команду: ");
+                (int cursorPositionLeft, int cursorPositionTop) = Console.GetCursorPosition();
+                if (message != null && message.Length > 0)
+                {
+                    Console.WriteLine($"\n\nИнформация:\n  {message}");
+                    message = null;
+                }
+                Console.WriteLine("\n");
+                Console.SetCursorPosition(0, cursorPositionTop + arrayDownPosition);
+                
+                Console.Write("Числа в массиве:");
+                foreach (int number in numbers)
+                {
+                    Console.Write($" {number}");
+                }
+                Console.SetCursorPosition(cursorPositionLeft, cursorPositionTop);
+
+                string userInput = Console.ReadLine();
+                if (int.TryParse(userInput, out int newNumber))
+                {
+                    int[] tmpNumbers = new int[numbers.Length + 1];
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        tmpNumbers[i] = numbers[i];
+                    }
+
+                    tmpNumbers[tmpNumbers.Length - 1] = newNumber;
+                    numbers = tmpNumbers;
+                    message = $"В конец массива было добавлено число {newNumber}";
+                    continue;
+                }
+                switch (userInput)
+                {
+                    case sumCommand:
+                        int sum = 0;
+                        foreach (int number in numbers)
+                        {
+                            sum += number;
+                        }
+
+                        message = $"Сумма всех чисел в массиве равняется {sum}";
+                        break;
+                    case exitCommand:
+                        canExit = true;
+                        break;
+                    default:
+                        message = $"Вы можете вводить только целые числа, либо команды {sumCommand}, {exitCommand}";
+                        break;
+                }
+            }
         }
     }
 }
