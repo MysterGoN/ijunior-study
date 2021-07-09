2f83b75d3cb30f0c3d9a15b6d1158890359b82a2
Nikolay Granenkov <nickgran@yandex.ru>
2021-07-09 11:44:32 +0300

    add: module 3 task 1

--- CSharpJunior/_03_Arrays_01_TargetElement/Program.cs
+++ CSharpJunior/_03_Arrays_01_TargetElement/Program.cs
@@ -6,7 +6,45 @@ namespace _03_Arrays_01_TargetElement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int sumRow = 2;
+            const int multCol = 1;
+            const int randNumberMin = 1;
+            const int randNumberMax = 9;
+            Random rand = new Random();
+            int[,] matrix = new int[5, 7];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = rand.Next(randNumberMin, randNumberMax + 1);
+                }
+            }
+            
+            Console.WriteLine("Матрица:");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.Write("  ");
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            int sum = 0;
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                sum += matrix[sumRow - 1, j];
+            }
+            Console.Write($"\nСумма {sumRow} строки: {sum}");
+
+            int mult = 1;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                mult *= matrix[i, multCol - 1];
+            }
+            Console.Write($"\nПроизведение {multCol} колонки: {mult}");
         }
     }
 }
