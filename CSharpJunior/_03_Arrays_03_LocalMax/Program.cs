ed1fd8fa5e06c8f7a718cf713b806f9055081430
Nikolay Granenkov <nickgran@yandex.ru>
2021-07-09 12:39:00 +0300

    module 3 task 3

--- CSharpJunior/_03_Arrays_03_LocalMax/Program.cs
+++ CSharpJunior/_03_Arrays_03_LocalMax/Program.cs
@@ -6,7 +6,49 @@ namespace _03_Arrays_03_LocalMax
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int randNumberMin = 10;
+            const int randNumberMax = 99;
+            Random rand = new Random();
+            int[] numbers = new int[30];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rand.Next(randNumberMin, randNumberMax + 1);
+            }
+
+            Console.WriteLine("Последовательность чисел:");
+            Console.Write("  ");
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+            
+            Console.WriteLine("Локальные максимумы:");
+            Console.Write("  ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i == 0)
+                {
+                    if (numbers[i] >= numbers[i + 1])
+                    {
+                        Console.Write($" {numbers[i]}");
+                    }
+                } else if (i == numbers.Length - 1)
+                {
+                    if (numbers[i] >= numbers[i - 1])
+                    {
+                        Console.Write($" {numbers[i]}");
+                    }
+                }
+                else
+                {
+                    if (numbers[i] >= numbers[i - 1] && numbers[i] >= numbers[i + 1])
+                    {
+                        Console.Write($" {numbers[i]}");
+                    }
+                }
+            }
         }
     }
 }
