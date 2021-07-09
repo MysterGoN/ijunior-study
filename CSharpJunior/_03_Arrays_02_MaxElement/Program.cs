540b705d524c382c328d50409653d3e8de300c76
Nikolay Granenkov <nickgran@yandex.ru>
2021-07-09 11:44:39 +0300

    add: module 3 task 2

--- CSharpJunior/_03_Arrays_02_MaxElement/Program.cs
+++ CSharpJunior/_03_Arrays_02_MaxElement/Program.cs
@@ -6,7 +6,72 @@ namespace _03_Arrays_02_MaxElement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int randNumberMin = 10;
+            const int randNumberMax = 99;
+            int maxNumber = int.MinValue;
+            int maxNumberPosX = 0;
+            int maxNumberPosY = 0;
+            Random rand = new Random();
+            int[,] matrix = new int[10, 10];
+            int newMatrixPositionX = matrix.GetLength(1) * 3 + 5;
+            int newMatrixPositionY = 0;
+
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = rand.Next(randNumberMin, randNumberMax + 1);
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Исходная матрица:");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.Write("  ");
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (maxNumber < matrix[i, j])
+                    {
+                        maxNumber = matrix[i, j];
+                        maxNumberPosX = j;
+                        maxNumberPosY = i;
+                    }
+                }
+            }
+
+            matrix[maxNumberPosY, maxNumberPosX] = 0;
+            Console.WriteLine($"\nМаксимальное значение: {maxNumber} " +
+                              $"располагается по координатам x={maxNumberPosX}, y={maxNumberPosY}");
+            
+            Console.SetCursorPosition(newMatrixPositionX, newMatrixPositionY);
+            Console.WriteLine("Итоговая матрица:");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.SetCursorPosition(newMatrixPositionX, newMatrixPositionY + i + 1);
+                Console.Write("  ");
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == 0)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
         }
     }
 }
