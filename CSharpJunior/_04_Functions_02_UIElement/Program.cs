200ab5f7c571dfd3b081b636d6b0c183eaaaeabb
Nikolay Granenkov <nickgran@yandex.ru>
2021-07-16 15:26:20 +0300

    add: m4t2

--- CSharpJunior/_04_Functions_02_UIElement/Program.cs
+++ CSharpJunior/_04_Functions_02_UIElement/Program.cs
@@ -6,7 +6,34 @@ namespace _04_Functions_02_UIElement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            
+            DrawColoredBar(0, 0, 15, 69, "HP");
+            DrawColoredBar(0, 1, 10, 10, "MP", ConsoleColor.Blue);
+            DrawColoredBar(0, 2, 5, 70, "SP", ConsoleColor.Green);
+        }
+
+        static void DrawColoredBar(int positionLeft, int positionTop, int length, int percent, string prefix,
+            ConsoleColor fillColor = ConsoleColor.Red, ConsoleColor emptyColor = ConsoleColor.Black)
+        {
+            Console.SetCursorPosition(positionLeft, positionTop);
+            ConsoleColor backgroundColor = Console.BackgroundColor;
+            
+            Console.Write($"{prefix}: [");
+            int fillLength = Convert.ToInt32(length * (percent / 100.0));
+            Console.BackgroundColor = fillColor;
+            for (int i = 0; i < fillLength; i++)
+            {
+                Console.Write(" ");
+            }
+
+            Console.BackgroundColor = emptyColor;
+            for (int i = fillLength; i < length; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.BackgroundColor = backgroundColor;
+            Console.Write("]");
         }
     }
 }
