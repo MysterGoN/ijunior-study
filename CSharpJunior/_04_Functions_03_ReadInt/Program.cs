904bbd6db89c58b9d94fe5f98ea6d3b8e00b1422
Nikolay Granenkov <nickgran@yandex.ru>
2021-07-16 15:49:02 +0300

    add: m4t3

--- CSharpJunior/_04_Functions_03_ReadInt/Program.cs
+++ CSharpJunior/_04_Functions_03_ReadInt/Program.cs
@@ -6,7 +6,30 @@ namespace _04_Functions_03_ReadInt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.Clear();
+                if (TryGetNumberFromUser(out int number))
+                {
+                    Console.WriteLine($"Ура! Вам удалось ввести число: {number}");
+                    break;
+                }
+                Console.WriteLine("Вы ввели неправильное число, попробуйте ещё раз!");
+                Console.Write("Для продолжения нажмите любую клавишу...");
+                Console.ReadKey();
+            }
+        }
+
+        static bool TryGetNumberFromUser(out int number)
+        {
+            Console.Write("Введите число: ");
+            string userInput = Console.ReadLine();
+            if (int.TryParse(userInput, out number))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
