098f4d429d8e5eb71322fcf860ddd9188adb7c89
Nikolay Granenkov <nickgran@yandex.ru>
2021-07-19 11:20:04 +0300

    add: m5t1

--- CSharpJunior/_05_Collections_01_Dictionary/Program.cs
+++ CSharpJunior/_05_Collections_01_Dictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _05_Collections_01_Dictionary
 {
@@ -6,7 +7,42 @@ namespace _05_Collections_01_Dictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string exitWord = "exit";
+            Dictionary<string, string> englishRussianDictionary = new Dictionary<string, string>
+            {
+                {"gold", "золото"},
+                {"experience", "опыт"},
+                {"human", "человек"},
+                {"target", "цель"},
+                {"book", "книга"}
+            };
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Перед вами Англо-русский словарь. Введите слово на английском, " +
+                                  $"чтобы получить перевод. Чтобы выйти напишите слово '{exitWord}', " +
+                                  "а также оно переводится как 'выход' поэтому вам не нужно искать его в словаре.");
+                Console.Write($"Введите слово для перевода или '{exitWord}': ");
+                string word = Console.ReadLine();
+
+                if (word == exitWord)
+                {
+                    break;
+                }
+
+                if (word != null && englishRussianDictionary.TryGetValue(word, out string translation))
+                {
+                    Console.WriteLine($"Перевод слова {word} - {translation}");
+                }
+                else
+                {
+                    Console.WriteLine("Такого слова нет в словаре! Попробуйте ввести другое слово.");
+                }
+
+                Console.Write("Для продолжения нажмите любую кнопку...");
+                Console.ReadKey();
+            }
         }
     }
 }
