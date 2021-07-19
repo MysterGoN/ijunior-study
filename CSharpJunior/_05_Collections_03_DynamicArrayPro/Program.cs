fc1b17eb1f282c6753097954fd24075f53c44e18
Nikolay Granenkov <nickgran@yandex.ru>
2021-07-19 12:36:54 +0300

    add: m5t3

--- CSharpJunior/_05_Collections_03_DynamicArrayPro/Program.cs
+++ CSharpJunior/_05_Collections_03_DynamicArrayPro/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _05_Collections_03_DynamicArrayPro
 {
@@ -6,7 +8,40 @@ namespace _05_Collections_03_DynamicArrayPro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string sumCommand = "sum";
+            const string exitCommand = "exit";
+            List<int> numbers = new List<int>();
+            bool canExit = false;
+
+            while (!canExit)
+            {
+                Console.Clear();
+                Console.Write($"Введите целое число или команды '{sumCommand}', '{exitCommand}': ");
+                string userInput = Console.ReadLine();
+
+                if (exitCommand.Equals(userInput))
+                {
+                    canExit = true;
+                    continue;
+                }
+
+                if (sumCommand.Equals(userInput))
+                {
+                    int totalSum = numbers.Sum();
+                    Console.WriteLine($"Итоговая сумма равна {totalSum}");
+                } else if (userInput != null && int.TryParse(userInput, out int newNumber))
+                {
+                    numbers.Add(newNumber);
+                    Console.WriteLine($"В список было добавлено число {newNumber}.");
+                }
+                else
+                {
+                    Console.WriteLine($"Вы не ввели ни целое число ни команды '{sumCommand}', '{exitCommand}', " +
+                                      $"попробуйте ещё раз");
+                }
+
+                Console.ReadKey();
+            }
         }
     }
 }
