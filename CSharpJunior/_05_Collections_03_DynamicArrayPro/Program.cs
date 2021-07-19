706288baa0d757146c11885d6ce8d2e44449650e
Nikolay Granenkov <nickgran@yandex.ru>
2021-07-19 14:37:37 +0300

    fix: m5t3

--- CSharpJunior/_05_Collections_03_DynamicArrayPro/Program.cs
+++ CSharpJunior/_05_Collections_03_DynamicArrayPro/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace _05_Collections_03_DynamicArrayPro
 {
@@ -18,30 +17,47 @@ namespace _05_Collections_03_DynamicArrayPro
                 Console.Clear();
                 Console.Write($"Введите целое число или команды '{sumCommand}', '{exitCommand}': ");
                 string userInput = Console.ReadLine();
-
-                if (exitCommand.Equals(userInput))
+                switch (userInput)
                 {
-                    canExit = true;
-                    continue;
-                }
-
-                if (sumCommand.Equals(userInput))
-                {
-                    int totalSum = numbers.Sum();
-                    Console.WriteLine($"Итоговая сумма равна {totalSum}");
-                } else if (userInput != null && int.TryParse(userInput, out int newNumber))
-                {
-                    numbers.Add(newNumber);
-                    Console.WriteLine($"В список было добавлено число {newNumber}.");
-                }
-                else
-                {
-                    Console.WriteLine($"Вы не ввели ни целое число ни команды '{sumCommand}', '{exitCommand}', " +
-                                      $"попробуйте ещё раз");
+                    case sumCommand:
+                        int totalSum = CalculateSum(numbers);
+                        Console.WriteLine($"Итоговая сумма равна {totalSum}");
+                        break;
+                    case exitCommand:
+                        canExit = true;
+                        break;
+                    default:
+                        if (int.TryParse(userInput, out int newNumber))
+                        {
+                            AddNumber(numbers, newNumber);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Вы не ввели ни целое число ни команды '{sumCommand}', " +
+                                              $"'{exitCommand}', попробуйте ещё раз");
+                        }
+                        break;
                 }
 
                 Console.ReadKey();
             }
+        }
+
+        static void AddNumber(List<int> numbers, int newNumber)
+        {
+            numbers.Add(newNumber);
+            Console.WriteLine($"В список было добавлено число {newNumber}.");
+        }
+
+        static int CalculateSum(List<int> numbers)
+        {
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum;
         }
     }
 }
