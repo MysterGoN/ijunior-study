bc87a9eb77a626ee84f3a988d7ff7bdb67cf1862
Nikolay Granenkov <nickgran@yandex.ru>
2021-07-27 14:57:45 +0300

    add: m6t2

--- CSharpJunior/_06_OOP_02_WorkingWithProperties/Program.cs
+++ CSharpJunior/_06_OOP_02_WorkingWithProperties/Program.cs
@@ -6,7 +6,33 @@ namespace _06_OOP_02_WorkingWithProperties
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Player player = new Player('@', 5, 4);
+            Renderer renderer = new Renderer();
+            
+            renderer.DrawPlayer(player);
+        }
+    }
+
+    class Player
+    {
+        public char Symbol { get; }
+        public int X { get; }
+        public int Y { get; }
+
+        public Player(char symbol, int x, int y)
+        {
+            Symbol = symbol;
+            X = x;
+            Y = y;
+        }
+    }
+
+    class Renderer
+    {
+        public void DrawPlayer(Player player)
+        {
+            Console.SetCursorPosition(player.X, player.Y);
+            Console.Write(player.Symbol);
         }
     }
 }
