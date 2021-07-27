4a020a2fb80b92896e938ab2a9a8723cd64aae21
Nikolay Granenkov <nickgran@yandex.ru>
2021-07-27 14:40:07 +0300

    add: m6t1

--- CSharpJunior/_06_OOP_01_WorkingWithClasses/Program.cs
+++ CSharpJunior/_06_OOP_01_WorkingWithClasses/Program.cs
@@ -6,7 +6,30 @@ namespace _06_OOP_01_WorkingWithClasses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Player player = new Player("Bob", 100, 10);
+            player.ShowInfo();
+        }
+    }
+
+    class Player
+    {
+        private int _health;
+        public string Name { get; private set; }
+        public int Money { get; private set; }
+
+        public Player(string name, int health, int money)
+        {
+            Name = name;
+            _health = health;
+            Money = money;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Параметры игрока:" +
+                              $"\n  Имя: {Name}" +
+                              $"\n  Здоровье: {_health}" +
+                              $"\n  Деньги: {Money}");
         }
     }
 }
