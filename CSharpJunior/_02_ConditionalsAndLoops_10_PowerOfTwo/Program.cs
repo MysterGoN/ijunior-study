92a27f0da3752baa29cfbd504721b94c70473b76
Nikolay Granenkov <nickgran@yandex.ru>
2022-07-14 22:44:31 +0300

    add: m2t10

---
+++ CSharpJunior/_02_ConditionalsAndLoops_10_PowerOfTwo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _02_ConditionalsAndLoops_10_PowerOfTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            
+            int minRandomNumber = 0;
+            int maxRandomNumber = 10000;
+            
+            Console.WriteLine("- Коллега, а давайте с вами поиграем в игру, я назову вам число, " +
+                              "а вам нужно бдует найти ближайшее большее число, которое при этом еще " +
+                              "и будет степенью двойки! Как вам такая идея?");
+            
+            Console.WriteLine("- Ну задавай коль не шутишь. - c усмешкой я ему ответил.");
+
+            int randomNumber = random.Next(minRandomNumber, maxRandomNumber + 1);
+            
+            Console.WriteLine($"- Хм... дай как подумать, как на счет - {randomNumber}?");
+
+            int power = 1;
+            int powerOfTwo = 2;
+            bool canExit = false;
+            while (!canExit)
+            {
+                if (powerOfTwo > randomNumber)
+                {
+                    canExit = true;
+                }
+                else
+                {
+                    powerOfTwo *= 2;
+                    power++;
+                }
+            }
+            
+            Console.WriteLine($"- Cтепень равна: {power}, а число {powerOfTwo}. " +
+                              "- Недолго думая ответил я.");
+            
+        }
+    }
+}
