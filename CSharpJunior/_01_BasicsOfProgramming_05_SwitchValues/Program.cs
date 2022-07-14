4d9c09168b1f962ec16e1e26d9f9aa64583c3d0f
Nikolay Granenkov <nickgran@yandex.ru>
2022-07-14 15:01:58 +0300

    add: m1t5

---
+++ CSharpJunior/_01_BasicsOfProgramming_05_SwitchValues/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01_BasicsOfProgramming_05_SwitchValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = "Stockholm";
+            string city = "Sweden";
+            
+            Console.WriteLine("Откуда вы прибыли в нашу прекрасную Арстоцку?\n" +
+                              $"Страна: {country}, Город: {city}.");
+            Console.WriteLine("Вы явно что-то напутали, проверьте пожалуйста еще раз!");
+
+            (city, country) = (country, city);
+
+            Console.WriteLine($"Страна: {country}, Город: {city}.");
+            Console.WriteLine("Вот теперь все корректно!");
+        }
+    }
+}
