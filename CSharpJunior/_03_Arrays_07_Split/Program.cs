9fe816fd0f615e2d7102ae81d1c14709a2eb4522
Nikolay Granenkov <nickgran@yandex.ru>
2022-07-16 16:51:38 +0300

    add: m3t7

---
+++ CSharpJunior/_03_Arrays_07_Split/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03_Arrays_07_Split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите предложение: ");
+            string inputPhrase = Console.ReadLine();
+
+            Console.WriteLine("Результирующее предложение:");
+            foreach (string word in inputPhrase.Split(' '))
+            {
+                Console.WriteLine(" " + word);
+            }
+        }
+    }
+}
